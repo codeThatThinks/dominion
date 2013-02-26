bcafd28385d2a5a3fd7a9b5fa1ccf9c4ab5e45b3
luntdotp <peter@snapki.net>
2013-02-26 17:42:32 +0000

    Added basic login area with show and hide for logged in

---
+++ scripts/login.js
@@ -0,0 +1,17 @@
+/**********
+ * Territory Functions
+ * Author: Peter Lunt
+ *********/
+
+//This stuff needs to be run constantly, a kind of loop I guess...
+
+$(document).ready(function(){
+
+if(loggedIn == true) {
+	$("#login").hide();
+}
+else {
+	$("#login").show();
+}
+
+});
