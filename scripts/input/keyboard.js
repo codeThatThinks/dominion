bfb44b699d917cdfc6f765dcd171cb942a57020e
bglen <br.glen@yahoo.com>
2013-03-02 10:39:31 -0500

    Input fixes, improved keyboard panning.

--- scripts/input/keyboard.js
+++ scripts/input/keyboard.js
@@ -3,71 +3,84 @@
  * Author: Peter Lunt
  *********/
 
-$(document).keyup(function(e)
+$(document).ready(function()
 {
-	if(allowInput)
+	$(document).keyup(function(e)
 	{
-		if(e.keyCode == 88 && !isPanning)								// When X key is pressed enable panning mode.
+		if(allowInput)
 		{
-			isClaiming = false;
-			getElement('btnClaim').color = new Color(0,0,0);
-
-			isPanning = true;
-			getElement('btnPan').color = new Color(136,136,136);
-		}
-
-		if(e.keyCode == 67 && !isClaiming)								// When C key is pressed enable claiming mode.
-		{
-			isPanning = false;
-			getElement('btnPan').color = new Color(0,0,0);
-
-			isClaiming = true;
-			getElement('btnClaim').color = new Color(136,136,136);
-		}
-
-		if(e.keyCode == 49 || e.keyCode == 173)							// When 1 or - is pressed zoom out.
-		{
-			if((gridSpacing - 10) >= 20 && (gridSpacing - 10) <= 150)
+			if(e.keyCode == 88 && !isPanning)								// When X key is pressed enable panning mode.
 			{
-				gridSpacing -= 10;
+				isClaiming = false;
+				getElement('btnClaim').color = new Color(0,0,0);
+
+				isPanning = true;
+				getElement('btnPan').color = new Color(136,136,136);
+			}
+
+			if(e.keyCode == 67 && !isClaiming)								// When C key is pressed enable claiming mode.
+			{
+				isPanning = false;
+				getElement('btnPan').color = new Color(0,0,0);
+
+				isClaiming = true;
+				getElement('btnClaim').color = new Color(136,136,136);
+			}
+
+			if(e.keyCode == 49 || e.keyCode == 173)							// When 1 or - is pressed zoom out.
+			{
+				if((gridSpacing - 10) >= 20 && (gridSpacing - 10) <= 150)
+				{
+					gridSpacing -= 10;
+				}
+			}
+
+			if(e.keyCode == 50 || e.keyCode == 61)							// When 2 or = is pressed zoom in.
+			{
+				if((gridSpacing + 10) >= 20 && (gridSpacing + 10) <= 150)
+				{
+					gridSpacing += 10;
+				}
+			}
+			
+			if(e.keyCode == 82)												//R Key centres the map			
+			{
+				origin.set(0 + (canvas.width() / 2), 0 + (canvas.height() / 2));
 			}
 		}
+	});
 
-		if(e.keyCode == 50 || e.keyCode == 61)							// When 2 or = is pressed zoom in.
+	$(document).keydown(function(e)
+	{
+		if(allowInput)
 		{
-			if((gridSpacing + 10) >= 20 && (gridSpacing + 10) <= 150)
+
+			/**
+			 * Handles camera movement
+			 */
+
+			if(e.keyCode == 65 || e.keyCode == 37)							//Camera  Left W					
 			{
-				gridSpacing += 10;
+				origin.set(origin.x + 55, origin.y);
 			}
+			
+			if(e.keyCode == 68 || e.keyCode == 39)							//Camera Right D						
+			{
+				origin.set(origin.x - 55, origin.y);
+			}
+			
+			if(e.keyCode == 83 || e.keyCode == 40)							//Camera Down W					
+			{
+				origin.set(origin.x, origin.y - 55);
+			}
+			
+			if(e.keyCode == 87 || e.keyCode == 38)							//Camera Up D				
+			{
+				origin.set(origin.x, origin.y + 55);
+			}
+
 		}
-		
-	/**
-	 * Handles camera movement
-	 */
-		
-		if(e.keyCode == 65 || e.keyCode == 37)							//Camera  Left W					
-		{
-			origin.set(origin.x + 70, origin.y);
-		}
-		
-		if(e.keyCode == 68 || e.keyCode == 39)							//Camera Right D						
-		{
-			origin.set(origin.x - 70, origin.y);
-		}
-		
-		if(e.keyCode == 83 || e.keyCode == 40)							//Camera Down W					
-		{
-			origin.set(origin.x, origin.y - 70);
-		}
-		
-		if(e.keyCode == 87 || e.keyCode == 38)							//Camera Up D				
-		{
-			origin.set(origin.x, origin.y + 70);
-		}
-		
-		if(e.keyCode == 82)												//R Key centres the map			
-		{
-			origin.set(0 + (canvas.width() / 2), 0 + (canvas.height() / 2));
-		}
-	}
+
+
+	});
 });
